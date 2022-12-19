09db067e46a286552ba05cef0f4c237ba98ca6d1
Jason Carmichael <jasonc0308@icloud.com>
2022-12-19 10:00:52 -0600

    nested footer into app js

--- venue-events/src/App.js
+++ venue-events/src/App.js
@@ -1,17 +1,23 @@
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <div className = "header">
-           <Header />
-           </div>
-
-            <div className = "main">
-            <Main />
+          <Header />
         </div>
+
+        <div className = "main">
+          <Main />
+        </div>
+
+        <div className = "footer">
+          <Footer />
+        </div>
+
     </div>
   );
 }
