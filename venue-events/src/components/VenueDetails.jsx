f83483ede639d45014db6a4925f3540493ff90b1
Jason Carmichael <jasonc0308@icloud.com>
2022-12-19 15:23:57 -0600

    venue details page working

---
+++ venue-events/src/components/VenueDetails.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+
+export default function VenueDetails (props) {
+    let { id } = useParams()
+    console.log(id)
+
+    const [venue, setVenue] = useState('')
+
+    const getData = async () => {
+        await axios.get(`http://localhost:8000/venues/${id}`)
+        .then(res => {
+            if (res) {
+                setVenue(res.data)
+                console.log(res.data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return venue ?(
+        <div className="events-wrapper">
+            <div className="venue-events-header">
+                <h2>Upcoming Events at {venue.name}</h2>
+            </div>
+                <p>Address: {venue.address}</p>
+                <p>Description: {venue.description}</p>
+                <p>Onsite Parking: {venue.onsite_parking}</p>
+                <p>Capacity: {venue.capacity}</p>
+                <p>Photo: {venue.photo_url}</p>
+                <img src={venue.photo_url}></img>
+                <br />
+
+        </div>
+
+    ) : null
+}
