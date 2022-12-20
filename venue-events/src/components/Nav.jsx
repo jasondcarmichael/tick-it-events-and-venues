df5f18f80e519ca84cca290852a4b7161802ea36
Jason Carmichael <jasonc0308@icloud.com>
2022-12-20 13:30:52 -0600

    css venue cards home

--- venue-events/src/components/Nav.jsx
+++ venue-events/src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav () {
         <div className="nav">
             <div className="nav-bar">
             <Link className="nav-home"to ="/">Home</Link>
-            <Link className="nav-venues"to="/venues">Venues</Link>
+            {/* <Link className="nav-venues"to="/venues">Venues</Link> */}
             <Link className="nav-events"to="/events">Events</Link>
             </div>
         </div>
